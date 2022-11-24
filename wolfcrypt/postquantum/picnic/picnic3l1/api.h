8556294d6c7ba0f6231b72bcd8bef329b9b60c93
g_tasop <g.tasop@protonmail.com>
2022-11-24 16:54:55 +0200

    Added third-party licenses

--- wolfcrypt/postquantum/picnic/picnic3l1/api.h
+++ wolfcrypt/postquantum/picnic/picnic3l1/api.h
@@ -1,6 +1,10 @@
 #ifndef PICNIC3_L1_FS_API_H
 #define PICNIC3_L1_FS_API_H
 
+ /* Modified by George Tasopoulos - ISI / Athena RC
+  * in order to be used in TLS 1.3.
+  */
+
 #define CRYPTO_SECRETKEYBYTES (1 + 2 * 17 + 17)
 #define CRYPTO_PUBLICKEYBYTES (1 + 2 * 17)
 #define CRYPTO_BYTES (4 + 14608)
@@ -26,9 +30,5 @@ int picnic_crypto_sign_verify(
     const unsigned char *m, size_t mlen, const unsigned char *pk);
 
 /* ----------------------------------------------------------------------- */
-
- // This version is from pqm4's opt version that is plain C but optimized.
- // I added the extra functions to be used in TLS.
- // PQClean does not include picnic and Microsoft's version is too complicated (?)
 
 #endif
